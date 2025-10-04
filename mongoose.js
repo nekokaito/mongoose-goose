7aef8f5d4ddf001b4de28f94c7884e1b9688ba44
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-10-04 12:51:38 +0600

    User Data with schema

--- mongoose.js
+++ mongoose.js
@@ -5,11 +5,23 @@ require("dotenv").config();
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASS;
 
-console.log("Hello Mongoose hi");
+const User = require("./schema");
 
 mongoose
   .connect(
     `mongodb+srv://${username}:${password}@cluster0.vrlyepl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => console.log("connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error(err));
+
+const run = async () => {
+  const userData = new User({
+    name: "Kaito",
+    age: 25,
+    email: "siddiq@gmail.com",
+  });
+
+  await userData.save().then(() => console.log("Data Saved"));
+};
+
+run();
