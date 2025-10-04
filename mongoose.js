aca2abce5140ceaaa3a2bf05fe7e1122fdf2471d
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-10-04 11:52:50 +0600

    mongoose connection

--- mongoose.js
+++ mongoose.js
@@ -1,1 +1,15 @@
+const { default: mongoose } = require("mongoose");
+
+require("dotenv").config();
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASS;
+
 console.log("Hello Mongoose hi");
+
+mongoose
+  .connect(
+    `mongodb+srv://${username}:${password}@cluster0.vrlyepl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => console.log("connected"))
+  .catch((err) => console.log(err));
